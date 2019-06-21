c8b7b550a3b637483cd7092a9a2ae8647d0e9c1d
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 20:50:58 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@ class Pokemon
       FROM pokemon
       WHERE id = ?
     SQL
-    db.execute(sql, id).collect{|row| self.new(id: row[0], name: row[1], type: row[2], db: db)}.first
+    db.execute(sql, id).collect{|row| self.new(id: row[0], name: row[1], type: row[2], db: db, hp: 60)}.first
   end
 
   def self.save(name, type, db)
